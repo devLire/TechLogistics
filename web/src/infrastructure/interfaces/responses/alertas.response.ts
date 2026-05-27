0f3ffd53e30e2fbd8616cec70a24c03719f7d5e5
Baruc Alejandria <alejandriacbaruc@gmail.com>
2026-05-26 22:08:07 -0500

    Add errors field to response interfaces for improved error handling

--- web/src/infrastructure/interfaces/responses/alertas.response.ts
+++ web/src/infrastructure/interfaces/responses/alertas.response.ts
@@ -1,30 +1,33 @@
+import type { ErrorsDetails } from '@/infrastructure/interfaces/error-details.interfaces.ts';
+
 export interface AlertasResponse {
-    status:  string;
-    message: string;
-    data:    Datum[];
-    count:   number;
+  status: string;
+  message: string;
+  data: Datum[];
+  errors: ErrorsDetails[];
+  count: number;
 }
 
 export interface Datum {
-    id_producto:   number;
-    id_categoria:  number;
-    id_proveedor:  number;
-    codigo_barras: string;
-    nombre:        string;
-    descripcion:   string;
-    precio_venta:  string;
-    stock_actual:  number;
-    stock_minimo:  number;
-    activo:        boolean;
-    proveedor:     Proveedor;
-    categoria:     Categoria;
+  id_producto: number;
+  id_categoria: number;
+  id_proveedor: number;
+  codigo_barras: string;
+  nombre: string;
+  descripcion: string;
+  precio_venta: string;
+  stock_actual: number;
+  stock_minimo: number;
+  activo: boolean;
+  proveedor: Proveedor;
+  categoria: Categoria;
 }
 
 export interface Categoria {
-    nombre: string;
+  nombre: string;
 }
 
 export interface Proveedor {
-    nombre_empresa: string;
-    telefono:       string;
+  nombre_empresa: string;
+  telefono: string;
 }
